7931a5cf2d770b23657051fc3808ffbd151cd266
Eduardo Grados <eduardogrados2@gmail.com>
2022-12-27 12:13:45 -0500

    ultimo coommit

--- shmup/__main__.py
+++ shmup/__main__.py
@@ -1,7 +1,9 @@
 import sys
 
 def main():
-    print("Hello")
+    print("Print de main")
 
 if __name__ == '__main__':
+    print ("print de if name = main")
     sys.exit(main())
+#me quedo en minuto 41:55 del video
